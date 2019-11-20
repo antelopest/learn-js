919b044e6d071863f9e9c3287711e9435d8d0fc0
Antelope <webantelopestudio@gmail.com>
2019-11-20 10:31:45 +0300

    modified 03-arguments.js

--- Композиция функции (pipe, compose) в JS/03-arguments.js
+++ Композиция функции (pipe, compose) в JS/03-arguments.js
@@ -7,17 +7,41 @@
 * REST наоборот, для соединения отдельных значений в массив
 * */
 
+/*
+* Pipe
+* - это канал, который связывает поток для чтения и поток для записи и позволяет сразу читать из потока
+* чтения в поток записи.
+* */
+
 const compose = (f, g) => (...args) => f(g(...args));
 const pipe = (f, g) => (...args) => g(f(...args));
 
 // Usage
 
-const upperFirst = word => word.charAt(0).toUpperCase() + word.slice(1);
-const upperCapital = s => s.split(' ').map(upperFirst).join(' ');
-const lower = s => s.toLowerCase();
+{
+  const upperFirst = word => word.charAt(0).toUpperCase() + word.slice(1);
+  const upperCapital = s => s.split(' ').map(upperFirst).join(' ');
+  const lower = s => s.toLowerCase();
 
-const s = 'ANTELOPE HOSTING FAST VPS FASTVPS';
-console.log(s);
-console.log(`lower('${s}) = '${upperCapital(s)}'`);
+  const s = 'ANTELOPE HOSTING FAST VPS FASTVPS';
+  console.log(s);
+  console.log(`lower('${s}) = '${upperCapital(s)}'`);
+
+  {
+    // Справа налево
+    console.log('Use compose');
+    const capitalize = compose(upperCapital, lower);
+    console.log(`capitalize(${s}) = '${capitalize(s)}'`);
+  }
+  {
+    // Слева направо
+    console.log('Use pipe');
+    const capitalize = pipe(lower, upperCapital);
+    console.log(`capitalize(${s}) = '${capitalize(s)}'`);
+  }
+}
+
+
+
 
 
