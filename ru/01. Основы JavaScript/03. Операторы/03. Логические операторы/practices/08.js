6b13e08833d05c7752930689f562226b2a465e23
antelopest <antelopest@gmail.com>
2025-07-17 21:24:03 +0700

    main add hasSendMessage

--- ru/01. Основы JavaScript/03. Операторы/03. Логические операторы/practices/08.js
+++ ru/01. Основы JavaScript/03. Операторы/03. Логические операторы/practices/08.js
@@ -1,1 +1,9 @@
 /* Задача 8 */
+
+/* Доступ в чат */
+
+/* Писать в чат можно, если пользователь онлайн и (он друг или модератор) */
+
+const hasSendMessage = (isOnline, isFriend, isModerator) => {
+    return isOnline && (isFriend || isModerator);
+}
