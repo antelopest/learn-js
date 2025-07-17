9b508e5e16365c5dc8b9a252458d14069b23833e
antelopest <antelopest@gmail.com>
2025-07-17 21:54:36 +0700

    main add practices

---
+++ ru/01. Основы JavaScript/03. Операторы/03. Логические операторы/practices/10.js
@@ -0,0 +1,8 @@
+/* Сокращенная проверка с || */
+
+/* Используя ||, запиши в fullName имя, если оно не пустое */
+
+const firstName = "";
+const lastName = "Smith";
+
+const fullName = firstName || lastName;
