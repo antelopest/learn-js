aca5c5d0f4328fd8810f870e77bd0ed2af776a51
antelopest <antelopest@gmail.com>
2025-08-13 16:23:43 +0700

    main change 05.js

--- interview/13-August-2025/04.js
+++ interview/13-August-2025/04.js
@@ -1,1 +1,52 @@
 // Задача 4
+
+// Функция должна откладывать выполнение переданной функции, пока не пройдет заданное время после последнего вызова.
+
+function debounce(fn, delay) {
+
+    let timeoutId;
+
+    return function (...args) {
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            fn.apply(this, args);
+        }, delay);
+    }
+}
+
+const log = debounce(() => console.log('Hello'), 1000);
+
+log();
+log();
+log();
+log();
+log(); // Выполнится только один раз через 1 секунду после последнего вызова
+
+// debounce с отменой
+
+function debounce1(fn, delay) {
+    let timeoutId;
+
+    function debounced(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            fn.apply(this, args);
+        }, delay);
+    }
+
+    debounced.cancel = () => {
+        clearTimeout(timeoutId);
+    }
+
+    return debounced;
+}
+
+const log1 = debounce1(() => console.log('Hello'), 1000);
+
+log1();
+log1();
+log1();
+log1();
+
+log1.cancel();
--- interview/13-August-2025/05.js
+++ interview/13-August-2025/05.js
@@ -1,1 +1,22 @@
 // Задача 5
+
+// Имитация работы Array.prototype.map
+
+// Напиши свою реализацию метода `map`, который повторяет поведение встроенного.
+
+function myMap(arr, callback, thisArg) {
+    const result = new Array(arr.length);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i in arr) { // пропуск пустых слотов
+            result[i] = callback.call(thisArg, arr[i], i, arr);
+        }
+
+    }
+
+    return result;
+}
+
+// Пример:
+const result = myMap([1, 2, 3], num => num * 2);
+console.log(result); // [2, 4, 6]
