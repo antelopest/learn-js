93c2b9547621701ff8c2fbea0e2ad542c89aad30
antelopest <antelopest@gmail.com>
2025-08-13 15:22:15 +0700

    main add 13-August-2025.md

---
+++ interview/13-August-2025/01.js
@@ -0,0 +1,28 @@
+// Развернуть строку без использования встроенного метода reverse()
+
+// Пример:
+// input: "JavaScript"
+// output: "tpircSavaJ"
+
+function reverseString(str) {
+    let array = [];
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        array.push(str[i]);
+    }
+
+    return array.join('');
+}
+
+let str1 = "JavaScript";
+let str2 = "HelloWorld";
+
+console.log(reverseString(str1)); // "tpircSavaJ"
+console.log(reverseString(str2)); // "dlroWolleH"
+
+function reverseString1(str) {
+    return str.split('').reduce((acc, cur) => cur + acc, '');
+}
+
+console.log(reverseString1(str1)); // "tpircSavaJ"
+console.log(reverseString1(str2)); // "dlroWolleH"
